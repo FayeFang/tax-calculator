b6434824940ed9351fd7a183b7552f64a41cc305
Faye Fang <faye.fang.33@gmail.com>
2016-05-27 14:23:41 -0500

    updated tax suummary

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -7,6 +7,10 @@ class ContentController < ApplicationController
 
   def summary
     @lookup_hash = lookup_hash
+    @team1_tax = calculate_tax(@lookup_hash[:team1], @lookup_hash[:filter], @lookup_hash[:income])
+    @team2_tax = calculate_tax(@lookup_hash[:team2], @lookup_hash[:filter], @lookup_hash[:income])
+    @winner = @team1_tax[:net_income] > @team2_tax[:net_income] ? @team1_tax : @team2_tax
+    @loser = @team1_tax[:net_income] > @team2_tax[:net_income] ? @team2_tax : @team1_tax
   end
 
   private def lookup_hash
